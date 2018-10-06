597e7650bc3e1e3630b930a24a67bb61ec90bc55
YaoShangTseng <yakushou730@gmail.com>
2018-10-06 21:49:37 +0800

    Lv1-5

--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
 type hotdog int
 
 var x hotdog
+var y int
 
 func main() {
 	fmt.Println(x)
 	fmt.Printf("%T\n", x)
 	x = 42
 	fmt.Println(x)
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
 }
