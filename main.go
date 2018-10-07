335597f661018e49ac7d4aa8edfe9d1fbfe369be
YaoShangTseng <yakushou730@gmail.com>
2018-10-07 09:33:16 +0800

    Lv2-2

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import (
 )
 
 func main() {
-	x := 42
-	fmt.Printf("%d\t%b\t%#x\n", x, x, x)
+	a := (42 == 42)
+	b := (42 <= 43)
+	c := (42 >= 43)
+	d := (42 != 43)
+	e := (42 < 43)
+	f := (42 > 43)
+	fmt.Println(a, b, c, d, e, f)
 }
