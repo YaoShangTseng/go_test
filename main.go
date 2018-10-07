f3cc1ae8eddf2bbdf8a69c1507e14c6857fcbd2f
YaoShangTseng <yakushou730@gmail.com>
2018-10-07 09:29:34 +0800

    Lv2-1

--- main.go
+++ main.go
@@ -4,17 +4,7 @@ import (
 	"fmt"
 )
 
-type hotdog int
-
-var x hotdog
-var y int
-
 func main() {
-	fmt.Println(x)
-	fmt.Printf("%T\n", x)
-	x = 42
-	fmt.Println(x)
-	y = int(x)
-	fmt.Println(y)
-	fmt.Printf("%T\n", y)
+	x := 42
+	fmt.Printf("%d\t%b\t%#x\n", x, x, x)
 }
