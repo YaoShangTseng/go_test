f6397439a12e981b0e9129ba931acc7674916932
YaoShangTseng <yakushou730@gmail.com>
2018-10-08 00:24:48 +0800

    Lv3-1

--- main.go
+++ main.go
@@ -4,16 +4,8 @@ import (
 	"fmt"
 )
 
-const (
-	a = 2017 + iota
-	b = 2017 + iota
-	c = 2017 + iota
-	d = 2017 + iota
-)
-
 func main() {
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
-	fmt.Println(d)
+	for i := 1; i <= 10000; i++ {
+		fmt.Println(i)
+	}
 }
