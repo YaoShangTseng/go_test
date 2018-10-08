9fd54cecf5f270e34d511104f7c9707887f68577
YaoShangTseng <yakushou730@gmail.com>
2018-10-09 00:00:08 +0800

    Lv4-3

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 
 func main() {
 	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
-	for i, v := range x {
-		fmt.Println(i, v)
-	}
-	fmt.Printf("%T\n", x)
+	fmt.Println(x[:5])
+	fmt.Println(x[5:])
+	fmt.Println(x[2:7])
+	fmt.Println(x[1:6])
 }
