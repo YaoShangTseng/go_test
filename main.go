ec10bfd27ca8587a08414a1bba40081290d4e47d
YaoShangTseng <yakushou730@gmail.com>
2018-10-11 00:12:57 +0800

    Lv4-8

--- main.go
+++ main.go
@@ -3,19 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	xs1 := []string{"James", "Bond", "Shaken, not stirred"}
-	xs2 := []string{"Miss", "Moneypenny", "Helloooooo, James."}
+	m := map[string][]string{
+		`bond_james`:      []string{`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: []string{`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           []string{`Being evil`, `Ice cream`, `Sunsets`},
+	}
 
-	fmt.Println(xs1)
-	fmt.Println(xs2)
+	// fmt.Println(m)
 
-	xxs := [][]string{xs1, xs2}
-	fmt.Println(xxs)
-
-	for i, xs := range xxs {
-		fmt.Println("record: ", i)
-		for j, val := range xs {
-			fmt.Printf("\tindex position: %v \t value: \t%v\n", j, val)
+	for k, v := range m {
+		fmt.Println("This is the record for", k)
+		for i, v2 := range v {
+			fmt.Println("\t", i, v2)
 		}
 	}
 }
