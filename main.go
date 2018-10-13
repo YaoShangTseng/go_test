0fa9c83a9884f42d7ffaa2bb67514a5e52fc69f0
Shou Tseng <shou.tseng@honestbee.com>
2018-10-13 15:58:56 +0800

    Lv5-2

--- main.go
+++ main.go
@@ -24,15 +24,19 @@ func main() {
 			"strawberry", "vanilla", "capuccino",
 		},
 	}
-	fmt.Println(p1.first)
-	fmt.Println(p1.last)
-	for i, v := range p1.favFlavors {
-		fmt.Println(i, v)
+
+	m := map[string]person{
+		p1.last: p1,
+		p2.last: p2,
 	}
 
-	fmt.Println(p2.first)
-	fmt.Println(p2.last)
-	for i, v := range p2.favFlavors {
-		fmt.Println(i, v)
+	for k, v := range m {
+		fmt.Println(k)
+		fmt.Println(v.first)
+		fmt.Println(v.last)
+		for i, val := range v.favFlavors {
+			fmt.Println(i, val)
+		}
+		fmt.Println("-------")
 	}
 }
