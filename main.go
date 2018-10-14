23149bc3403fc6964a810cc750dd7c10e606469e
YaoShangTseng <yakushou730@gmail.com>
2018-10-14 16:55:48 +0800

    Lv6-2

--- main.go
+++ main.go
@@ -3,15 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	n := foo()
-	x, s := bar()
-	fmt.Println(n, x, s)
+	ii1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	n := foo(ii1...)
+	fmt.Println(n)
+
+	ii2 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	n2 := bar(ii2)
+	fmt.Println(n2)
 }
 
-func foo() int {
-	return 42
+func foo(xi ...int) int {
+	total := 0
+	for _, v := range xi {
+		total += v
+	}
+	return total
 }
 
-func bar() (int, string) {
-	return 1984, "Big Brother is Watching"
+func bar(xi []int) int {
+	total := 0
+	for _, v := range xi {
+		total += v
+	}
+	return total
 }
