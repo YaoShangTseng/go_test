54123800d8509f98005fa2067e74ff9c80e5aeed
YaoShangTseng <yakushou730@gmail.com>
2018-10-14 17:31:49 +0800

    Lv6-3

--- main.go
+++ main.go
@@ -1,29 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	ii1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	n := foo(ii1...)
-	fmt.Println(n)
-
-	ii2 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	n2 := bar(ii2)
-	fmt.Println(n2)
+	defer foo()
+	bar()
 }
 
-func foo(xi ...int) int {
-	total := 0
-	for _, v := range xi {
-		total += v
-	}
-	return total
+func foo() {
+	defer func() {
+		fmt.Println("foo DEFER ran")
+	}()
+	fmt.Println("foo ran")
 }
 
-func bar(xi []int) int {
-	total := 0
-	for _, v := range xi {
-		total += v
-	}
-	return total
+func bar() {
+	fmt.Println("BAR")
 }
