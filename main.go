d96a5fe2f76f7fb4c3972400e5ac9307b27daef7
YaoShangTseng <yakushou730@gmail.com>
2018-10-21 16:27:03 +0800

    Lv7-1

--- main.go
+++ main.go
@@ -5,22 +5,7 @@ import (
 )
 
 func main() {
-	f := foo()
-	fmt.Println(f())
-	fmt.Println(f())
-	g := foo()
-	fmt.Println(g())
-	fmt.Println(g())
-	fmt.Println(g())
-	fmt.Println(g())
-	fmt.Println(g())
-	fmt.Println(g())
-}
-
-func foo() func() int {
-	x := 0
-	return func() int {
-		x++
-		return x
-	}
+	x := 42
+	fmt.Println(x)
+	fmt.Println(&x)
 }
