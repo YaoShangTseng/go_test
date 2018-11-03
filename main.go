38820f603816770097e85f228a374b0daa5d009f
YaoShangTseng <yakushou730@gmail.com>
2018-11-03 09:01:39 +0800

    Lv8-3

--- main.go
+++ main.go
@@ -3,35 +3,55 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 )
 
-type person struct {
-	First   string   `json:"First"`
-	Last    string   `json:"Last"`
-	Age     int      `json:"Age"`
-	Sayings []string `json:"Sayings"`
+type user struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
 }
 
 func main() {
-	s := `[{"First":"James","Last":"Bond","Age":32,"Sayings":["Shaken, not stirred","Youth is no guarantee of innovation","In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27,"Sayings":["James, it is soo good to see you","Would you like me to take care of that for you, James?","I would really prefer to be a secret agent myself."]},{"First":"M","Last":"Hmmmm","Age":54,"Sayings":["Oh, James. You didn't.","Dear God, what has James done now?","Can someone please tell me where James Bond is?"]}]`
-
-	// https://mholt.github.io/json-to-go/
-
-	fmt.Println(s)
-
-	var people []person
-
-	err := json.Unmarshal([]byte(s), &people)
-	if err != nil {
-		fmt.Println(err)
+	u1 := user{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
 	}
-	fmt.Println(people)
 
-	for i, person := range people {
-		fmt.Println("Person #", i)
-		fmt.Println("\t", person.First, person.Last, person.Age)
-		for _, saying := range person.Sayings {
-			fmt.Println("\t\t", saying)
-		}
+	u2 := user{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
+
+	u3 := user{
+		First: "M",
+		Last:  "Hmmmm",
+		Age:   54,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
+
+	users := []user{u1, u2, u3}
+	fmt.Println(users)
+
+	err := json.NewEncoder(os.Stdout).Encode(users)
+	if err != nil {
+		fmt.Println("We did something wrong and here's the error:", err)
 	}
 }
