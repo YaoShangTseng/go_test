23572cf0f49390e22a20c07f85ce0cfe3de92a1a
YaoShangTseng <yakushou730@gmail.com>
2018-11-03 15:16:22 +0800

    Lv9-1

--- main.go
+++ main.go
@@ -2,91 +2,30 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	"runtime"
+	"sync"
 )
 
-type user struct {
-	First   string
-	Last    string
-	Age     int
-	Sayings []string
-}
-
-type ByAge []user
-
-func (a ByAge) Len() int           { return len(a) }
-func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
-
-type ByLast []user
-
-func (l ByLast) Len() int           { return len(l) }
-func (l ByLast) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
-func (l ByLast) Less(i, j int) bool { return l[i].Last < l[j].Last }
-
 func main() {
-	u1 := user{
-		First: "James",
-		Last:  "Bond",
-		Age:   32,
-		Sayings: []string{
-			"Shaken, not stirred",
-			"Youth is no guarantee of innovation",
-			"In his majesty's royal service",
-		},
-	}
+	fmt.Println("begin CUP", runtime.NumCPU())
+	fmt.Println("begin gs", runtime.NumGoroutine())
 
-	u2 := user{
-		First: "Miss",
-		Last:  "Moneypenny",
-		Age:   27,
-		Sayings: []string{
-			"James, it is soo good to see you",
-			"Would you like me to take care of that for you, James?",
-			"I would really prefer to be a secret agent myself.",
-		},
-	}
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		fmt.Println("hello from thing one")
+		wg.Done()
+	}()
+	go func() {
+		fmt.Println("hello from thing two")
+		wg.Done()
+	}()
 
-	u3 := user{
-		First: "M",
-		Last:  "Hmmmm",
-		Age:   54,
-		Sayings: []string{
-			"Oh, James. You didn't.",
-			"Dear God, what has James done now?",
-			"Can someone please tell me where James Bond is?",
-		},
-	}
+	fmt.Println("mid CUP", runtime.NumCPU())
+	fmt.Println("mid gs", runtime.NumGoroutine())
 
-	users := []user{u1, u2, u3}
-
-	fmt.Println(users)
-
-	for _, u := range users {
-		fmt.Println(u.First, u.Last, u.Age)
-		for _, v := range u.Sayings {
-			fmt.Println("\t", v)
-		}
-	}
-
-	fmt.Println("-------")
-
-	sort.Sort(ByAge(users))
-	for _, u := range users {
-		fmt.Println(u.First, u.Last, u.Age)
-		sort.Strings(u.Sayings)
-		for _, v := range u.Sayings {
-			fmt.Println("\t", v)
-		}
-	}
-
-	fmt.Println("-------")
-
-	sort.Sort(ByLast(users))
-	for _, u := range users {
-		fmt.Println(u.First, u.Last, u.Age)
-		for _, v := range u.Sayings {
-			fmt.Println("\t", v)
-		}
-	}
+	wg.Wait()
+	fmt.Println("about to exit")
+	fmt.Println("end CUP", runtime.NumCPU())
+	fmt.Println("end gs", runtime.NumGoroutine())
 }
